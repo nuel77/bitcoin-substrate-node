c87b933e45f722f9387f92b4b77fbc4934dc286d
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-06-19 03:19:21 +0530

    feat: add utxo logic

--- pallets/utxo/src/lib.rs
+++ pallets/utxo/src/lib.rs
@@ -1,95 +1,152 @@
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::dispatch::DispatchResult;
-	use frame_support::pallet_prelude::*;
-	use frame_system::ensure_signed;
-	use frame_system::pallet_prelude::*;
-	#[cfg(feature = "std")]
-	use serde::{Deserialize, Serialize};
-	use sp_core::crypto::AccountId32;
-	use sp_core::{H256, H512};
+    use sp_std::collections::btree_map::BTreeMap;
+    use codec::Codec;
+    use frame_support::dispatch::DispatchResult;
+    use frame_support::pallet_prelude::*;
+    use frame_system::ensure_signed;
+    use frame_system::pallet_prelude::*;
+    #[cfg(feature = "std")]
+    use serde::{Deserialize, Serialize};
+    use sp_core::{H256};
 
-	#[pallet::pallet]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		/// The overarching runtime event type.
-		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-	}
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching runtime event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+    }
 
-	#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-	#[derive(
-		Clone,
-		Encode,
-		Decode,
-		Eq,
-		PartialEq,
-		PartialOrd,
-		Ord,
-		RuntimeDebug,
-		Hash,
-		Default,
-		MaxEncodedLen,
-		TypeInfo,
-	)]
-	pub struct Utxo {
-		value: u64,
-		owner: H256,
-	}
+    #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+    #[derive(
+        Clone,
+        Encode,
+        Decode,
+        Eq,
+        PartialEq,
+        PartialOrd,
+        Ord,
+        RuntimeDebug,
+        Hash,
+        Default,
+        MaxEncodedLen,
+        TypeInfo,
+    )]
+    pub struct Utxo<AccountId: Parameter + Member + MaxEncodedLen> {
+        value: u64,
+        /// owner of the utxo accountID
+        owner: AccountId,
+    }
 
-	#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-	#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, Hash, Default, TypeInfo)]
-	pub struct Transaction {
-		/// id of the utxo to be spend
-		utxo_id: H256,
-		signature: H512,
-	}
+    #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+    #[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, Hash, Default, TypeInfo)]
+    pub struct TransactionInput {
+        /// id of the utxo to be spend
+        utxo_id: H256,
+    }
 
-	#[pallet::storage]
-	#[pallet::getter(fn utxo_store)]
-	pub(super) type UtxoStore<T: Config> =
-		StorageMap<_, Blake2_256, H256, Option<Utxo>, ValueQuery>;
+    #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+    #[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, Hash, Default, TypeInfo)]
+    pub struct Transaction<AccountId: Parameter + Member + MaxEncodedLen> {
+        pub(crate) inputs: Vec<TransactionInput>,
+        pub(crate) outputs: Vec<Utxo<AccountId>>,
+    }
 
-	#[pallet::event]
-	#[pallet::generate_deposit(pub (super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		/// A user has successfully transferred a utxo.
-		UtxoTransferred { who: T::AccountId },
-	}
+    #[pallet::storage]
+    #[pallet::getter(fn utxo_store)]
+    pub(super) type UtxoStore<T: Config> =
+    StorageMap<_, Blake2_256, H256, Option<Utxo<T::AccountId>>, ValueQuery>;
 
-	#[pallet::error]
-	pub enum Error<T> {
-		InvalidTransaction,
-	}
+    #[pallet::event]
+    #[pallet::generate_deposit(pub (super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// A user has successfully transferred a utxo.
+        UtxoTransferred,
+    }
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		#[pallet::call_index(0)]
-		#[pallet::weight(T::WeightInfo::do_something())]
-		pub fn transfer(origin: OriginFor<T>, transaction: Vec<Transaction>) -> DispatchResult {
-			//ensure_signed(&origin)?;
-			// Check that the extrinsic was signed and get the signer.
-			let is_valid = Self::validate_tx(&transaction)?;
+    #[pallet::error]
+    pub enum Error<T> {
+        DuplicatedTransaction,
+        InvalidTransaction,
+        OutputValueIsZero,
+    }
 
-			// Update storage.
-			Self::update_utxo_store(&transaction)?;
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        #[pallet::weight(100_000)]
+        pub fn transfer(origin: OriginFor<T>, transaction: Transaction<T::AccountId>) -> DispatchResult {
+            let who = ensure_signed(origin)?;
+            // Check that the extrinsic was signed and get the signer.
+            Self::validate_tx(&transaction, &who)?;
 
-			// Emit an event.
-			// Self::deposit_event(Event::UtxoTransferred { who: origin.into() });
+            // Update storage.
+            Self::update_utxo_store(&transaction)?;
 
-			// Return a successful `DispatchResult`
-			Ok(())
-		}
-	}
+            // Emit an event.
+            Self::deposit_event(Event::UtxoTransferred);
 
-	impl<T: Config> Pallet<T> {
-		pub fn validate_tx(transaction: &Vec<Transaction>) -> DispatchResult {
-			Ok(())
-		}
+            // Return a successful `DispatchResult`
+            Ok(())
+        }
+    }
 
-		pub fn update_utxo_store(transaction: &Vec<Transaction>) -> DispatchResult {
-			Ok(())
-		}
-	}
+    impl<T: Config> Pallet<T> {
+        pub fn validate_tx(transaction: &Transaction<T::AccountId>, who: &T::AccountId) -> Result<(), &'static str> {
+            ensure!(transaction.inputs.len() > 0, "Transaction input length is 0");
+            ensure!(transaction.outputs.len() > 0, "Transaction output length is 0");
+
+            {
+                // check if input transactions are not duplicated
+                let mut map = BTreeMap::new();
+                for input in &transaction.inputs {
+                    map.insert(input.utxo_id, ());
+                }
+                ensure!(map.len() == transaction.inputs.len(), "Duplicated Transaction Input");
+            }
+
+            {
+                // check if output transactions are not duplicated
+                let mut map = BTreeMap::new();
+                for output in &transaction.outputs {
+                    map.insert(&output.owner, ());
+                }
+                ensure!(map.len() == transaction.outputs.len(),"Duplicated Transaction Output");
+            }
+
+            // check if signature is valid and check if inputs are valid (exist in utxo store)
+            for input in &transaction.inputs {
+                if let Some(utxo) = UtxoStore::<T>::get(input.utxo_id) {
+                    if utxo.owner != *who {
+                        return Err("Owner of input and output does not match");
+                    }
+                } else {
+                    return Err("Input does not exist in UTXO store");
+                }
+            }
+
+            // validate if output are valid (non 0)
+            for output in &transaction.outputs {
+                ensure!(output.value > 0, "Output value is 0");
+            }
+            Ok(())
+        }
+
+        pub fn update_utxo_store(transaction: &Transaction<T::AccountId>) -> DispatchResult {
+            //remove outdated utxo
+            for input in &transaction.inputs {
+                UtxoStore::<T>::remove(input.utxo_id);
+            }
+
+            //add newly validated utxos
+            for output in &transaction.outputs {
+                let hash = H256::random();
+                UtxoStore::<T>::insert(hash, Some(output.clone()));
+            }
+
+            Ok(())
+        }
+    }
 }
