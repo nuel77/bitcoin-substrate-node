8052b1d6fd4845b7df0a9216a42a6f732abef80d
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2024-06-24 01:52:18 +0530

    chore: rename

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -98,8 +98,8 @@ pub mod opaque {
 // https://docs.substrate.io/main-docs/build/upgrade#runtime-versioning
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("bitcoin-substrate-node-runtime"),
-	impl_name: create_runtime_str!("bitcoin-substrate-node-runtime"),
+	spec_name: create_runtime_str!("bitcoin-substrate"),
+	impl_name: create_runtime_str!("bitcoin-substrate"),
 	authoring_version: 1,
 	// The version of the runtime specification. A full node will not attempt to use its native
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
